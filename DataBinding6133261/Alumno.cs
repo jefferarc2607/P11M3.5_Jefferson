99f2b88eb732d145297b1e0582b0f002a7308d63
Jefferson Arce <jeffersonarce439@gmail.com>
2026-09-11 19:17:45 -0600

    DataBinding Xamarin del video

---
+++ DataBinding6133261/Alumno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DataBinding6133261
+{
+    public class Alumno : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+                OnPropertyChanged(nameof(MostrarNombre));
+            }
+        }
+
+        public string MostrarNombre => $"Hola bienvenido: {Nombre}";
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DataBinding6133261/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DataBinding6133261
+{
+     public partial class MainPage : ContentPage
+     {
+         public MainPage()
+         {
+            InitializeComponent();
+            BindingContext = new Alumno();
+         }
+     }
+}
